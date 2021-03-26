20ee22d58b88bab967e6867e6a4b3a55be7bfcd2
bravejang <wkdrmfmt@gmail.com>
2021-03-26 11:52:44 +0900

    번역완료

--- accounts/models.py
+++ accounts/models.py
@@ -5,10 +5,10 @@ from django.db import models
 
 class User(AbstractUser):
     avatar = models.ImageField(
-        upload_to="accounts/%Y/%m", default="accounts/base_user.png"
+        upload_to="accounts/%Y/%m", default="accounts/base_user.png", verbose_name="프로필 사진"
     )
-    nickname = models.CharField(unique=True, max_length=10)
-    bio = models.CharField(blank=True, max_length=255)
+    nickname = models.CharField(unique=True, max_length=10, verbose_name="닉네임")
+    bio = models.CharField(blank=True, max_length=255, verbose_name="자기소개")
 
     def get_absolute_url(self):
         return reverse("accounts:profile", kwargs={"pk": self.pk})
